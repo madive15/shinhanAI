6fd69528223882ecac424d7ce3e955f1e6d68ed5
yc-jeon <yc.jeon@madive.co.kr>
2024-11-21 14:50:13 +0900

    admin layout 수정

--- src/publish/layout/AdminLayout.tsx
+++ src/publish/layout/AdminLayout.tsx
@@ -6,6 +6,7 @@ import Loading from "~/publish/loading/Loading";
 import Top from "~/publish/layout/Top";
 import MenuContainer from "~/publish/layout/MenuContainer";
 import AdminTab from "~/publish/components/AdminTab";
+import { ReactComponent as Home } from "~assets/images/svg/Icons-home.svg";
 
 // need style
 import "~/publish/layout/layout.scss";
@@ -52,13 +53,33 @@ const AdminLayout: React.FC<IPageProps> = () => {
                         <div className="sub-title">
                             <div className="tit-gnb-box">
                                 <span className="tit">서브 타이틀</span>
-                                <div>
-                                    <span></span>
-                                    <span>권한/회원관리</span>
-                                    <span>권한관리</span>
+                                <div className="nav">
+                                    <Home className="home" />
+                                    <span className="deps">권한/회원관리</span>
+                                    <span className="deps">
+                                        <strong>권한관리</strong>
+                                    </span>
                                 </div>
                             </div>
-                        </div> 
+                        </div>
+
+                        <div className="">
+                            <div className="">
+                                <span>조회항목</span>
+                                <div>
+                                    <select></select>
+                                    <input type="text" />
+                                </div>
+                            </div>
+                            <div className="">
+                                <span>권한구분</span>
+                                <div>
+                                    <select></select>
+                                    <input type="text" />
+                                </div>
+                            </div>
+                        </div>
+                        <div>권한목록</div>
                     </div>
                 </div>
             </div>
--- src/publish/layout/Top.tsx
+++ src/publish/layout/Top.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logoPath from "~assets/images/admin_logo.png";
 import { ReactComponent as Search } from "~assets/images/svg/Icons-search.svg";
+import { ReactComponent as Setting } from "~assets/images/svg/Icons-set.svg";
 import Button from "~/publish/components/Button";
 
 // Props type
@@ -31,7 +32,7 @@ const Top: React.FC<IPageProps> = (props) => {
     // }
 
     return (
-        <header className={""}>
+        <header>
             <h1 className="logo">
                 <NavLink to={"/"}>
                     <img src={logoPath} alt="logo" />
@@ -43,11 +44,16 @@ const Top: React.FC<IPageProps> = (props) => {
                     <Search className="icon-search" />
                 </label>
                 <div className="user-setting">
-                    <div>
+                    <div className="user-time">
                         <span>59:59</span>
                         <Button value="시간연장" viewType="type3" />
                     </div>
-                    <div>sett</div>
+                    <span className="nick-name">
+                        <em>김멀더(MulderKim)</em> 님
+                    </span>
+                    <span className="setting-box">
+                        <Setting className="icon-set" />
+                    </span> 
                 </div>
             </div>
         </header>
