cd1ff66ef872e65bca11489d39e18ee082c04050
ojs91 <js.oh@madive.co.kr>
2024-11-26 17:27:05 +0900

    퍼블 현황판 수정

--- ai-studio-admin/src/publish/index.tsx
+++ ai-studio-admin/src/publish/index.tsx
@@ -2,26 +2,72 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import "./pubilsh.scss";
+import { tr } from "date-fns/locale";
 
 export default function PubIndex() {
-    return (
-        <div className="index-wrap">
-            <div className="title">
-                <h1>퍼블리싱 작업 리스트</h1>
-            </div>
-
-            <ul className="index-list">
-                <li>
-                    <h2>
-                        <NavLink to={"/publish/guide"}>1. 가이드</NavLink>
-                    </h2>
-                </li>
-                <li>
-                    <h2>
-                        <NavLink to={"/publish/adminlayout"}>2. 어드민 레이아웃 샘플</NavLink>
-                    </h2>
-                </li>
-            </ul>
-        </div>
-    );
+  const pubListHeader = ["No", "화면아이디", "화면명", "링크", "비고"];
+  const pubList = [
+    {
+      id: "화면아이디",
+      name: "가이드",
+      link: "/publish/guide",
+      etc: "",
+    },
+    {
+      id: "화면아이디",
+      name: "권한관리",
+      link: "/publish/table",
+      etc: "",
+    },
+  ];
+  return (
+    <div className="index-wrap">
+      <div className="title">
+        <h1>퍼블리싱 BO작업 리스트</h1>
+      </div>
+      <table className="pub-list-table">
+        <colgroup>
+          <col style={{ width: "120px" }} />
+          <col style={{ width: "200px" }} />
+          <col style={{ width: "200px" }} />
+          <col style={{ width: "300px" }} />
+          <col style={{ width: "" }} />
+        </colgroup>
+        <thead>
+          <tr>
+            {pubListHeader.map((v, i) => (
+              <th key={i}>{v}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {pubList.map((v, i) => (
+            <tr key={i}>
+              <td>{i + 1}</td>
+              <td>{v.id}</td>
+              <td>{v.name}</td>
+              <td>
+                <NavLink target="_blank" to={v.link}>
+                  {v.link}
+                </NavLink>
+              </td>
+              <td>{v.etc}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {/* <ul className="index-list">
+        <li>
+          <h2>
+            <NavLink to={"/publish/guide"}>1. 가이드</NavLink>
+          </h2>
+        </li>
+        <li>
+          <h2>
+            <NavLink to={"/publish/table"}>2. 어드민 레이아웃 샘플</NavLink>
+          </h2>
+        </li>
+      </ul> */}
+    </div>
+  );
 }
