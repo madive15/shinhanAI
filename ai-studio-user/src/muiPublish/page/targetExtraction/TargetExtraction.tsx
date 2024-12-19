5c848f24b6d391547dedcfc738d7753ffde09a7a
yc-jeon <yc.jeon@madive.co.kr>
2024-12-19 09:59:11 +0900

    target  content layout

--- ai-studio-user/src/muiPublish/page/targetExtraction/TargetExtraction.tsx
+++ ai-studio-user/src/muiPublish/page/targetExtraction/TargetExtraction.tsx
@@ -5,6 +5,7 @@ import React, { useState, useEffect, useRef } from "react";
 import Loading from "~/muiPublish/loading/Loading";
 import Tab from "~/muiPublish/components/Tab";
 import SubTitle from "~/muiPublish/components/SubTitle";
+import { ReactComponent as Home } from "~assets/images/svg/Icons-home.svg";
 
 // need style
 import "~/muiPublish/layout/layout.scss";
@@ -52,60 +53,42 @@ const TargetExtraction: React.FC<IPageProps> = (props) => {
         "메뉴4",
         "메뉴2",
         "메뉴3",
-        "메뉴4",
-        "메뉴2",
-        "메뉴3",
-        "메뉴4",
-        "메뉴2",
-        "메뉴3",
-        "메뉴4",
-        "메뉴2",
-        "메뉴3",
-        "메뉴4",
-        "메뉴2",
-        "메뉴3",
-        "메뉴4",
-        "메뉴2",
-        "메뉴3",
-        "메뉴4",
-        "메뉴2",
-        "메뉴3",
-        "메뉴4",
-        "메뉴2",
-        "메뉴3",
-        "메뉴4",
-        "메뉴2",
-        "메뉴3",
-        "메뉴4",
-        "메뉴2",
-        "메뉴3",
-        "메뉴4",
-        "메뉴2",
-        "메뉴3",
-        "메뉴4",
-        "메뉴2",
-        "메뉴3",
-        "메뉴4",
     ];
-
-    // subtitle search height
-    const [heiTitSearch, setHeiTitSearch] = useState<number>(190);
-    const subTitSearch = useRef<HTMLDivElement | null>(null);
-    useEffect(() => {
-        if (subTitSearch.current) {
-            const offsetHeight = subTitSearch.current.offsetHeight;
-            const offsetWidth = subTitSearch.current.offsetWidth;
-            console.log("Height:", offsetHeight + 190, "Width:", offsetWidth);
-            setHeiTitSearch(offsetHeight + 190);
-        }
-    }, [subTitSearch?.current?.offsetHeight, heiTitSearch]);
 
     return (
         <>
             <div className="tabs-area">
                 <Tab menuName={adminMenu} />
             </div>
-            <div className="tabs-contents">content</div>
+            <div className="tabs-contents">
+                <div className="list-content">
+                    <div className="tit-gnb-box">
+                        <div className="nav">
+                            <Home className="home" />
+                            <span className="deps">{props.pageName}</span>
+                            <span className="deps">
+                                <strong>{props.pageName}</strong>
+                            </span>
+                        </div>
+                    </div>
+                    <div>
+                        <div>타겟 추출</div>
+                        <div>
+                            <span>프리빌트</span>
+                            <span>직접추출</span>
+                        </div>
+                        <div>
+                            <input type="text" />
+                        </div>
+                    </div>
+                </div>
+                <div className="list-setting">
+                    <div className="no-data">
+                        <strong>AI가 미리 생성해놓은 조건</strong>을 선택해서
+                        <br /> 타겟을 추출하세요.
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
