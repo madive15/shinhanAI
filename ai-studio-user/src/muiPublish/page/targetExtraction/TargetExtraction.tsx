b47b3185188af366c338704d3e177b53c6904df0
yc-jeon <yc.jeon@madive.co.kr>
2024-12-19 16:54:13 +0900

    target  content 1219

--- ai-studio-user/src/muiPublish/page/targetExtraction/TargetExtraction.tsx
+++ ai-studio-user/src/muiPublish/page/targetExtraction/TargetExtraction.tsx
@@ -4,8 +4,17 @@ import React, { useState, useEffect, useRef } from "react";
 // need content
 import Loading from "~/muiPublish/loading/Loading";
 import Tab from "~/muiPublish/components/Tab";
-import SubTitle from "~/muiPublish/components/SubTitle";
-import { ReactComponent as Home } from "~assets/images/svg/Icons-home.svg";
+import { ReactComponent as Home } from "~assets/images/svg/icon_navi_home.svg";
+import { ReactComponent as Search } from "~assets/images/svg/Icons-search.svg";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControl from "@mui/material/FormControl";
 
 // need style
 import "~/muiPublish/layout/layout.scss";
@@ -55,6 +64,16 @@ const TargetExtraction: React.FC<IPageProps> = (props) => {
         "메뉴3",
     ];
 
+    const [radioGroup, setRadioGroup] = React.useState("개인");
+    const radioGroupChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRadioGroup((event.target as HTMLInputElement).value);
+    };
+
+    const [radioGroup2, setRadioGroup2] = React.useState("인기/추천");
+    const radioGroupChange2 = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRadioGroup2((event.target as HTMLInputElement).value);
+    };
+
     return (
         <>
             <div className="tabs-area">
@@ -66,26 +85,74 @@ const TargetExtraction: React.FC<IPageProps> = (props) => {
                         <div className="nav">
                             <Home className="home" />
                             <span className="deps">{props.pageName}</span>
-                            <span className="deps">
-                                <strong>{props.pageName}</strong>
-                            </span>
+                            <span className="deps">{props.pageName}</span>
                         </div>
                     </div>
-                    <div>
-                        <div>타겟 추출</div>
-                        <div>
-                            <span>프리빌트</span>
-                            <span>직접추출</span>
+                    <div className="sub-title-tab-box">
+                        <div className="tit-tabs-box">
+                            <div className="sub-tit">타겟 추출</div>
+                            <div className="sub-tabs">
+                                <span className="active">프리빌트</span>
+                                <span>직접추출</span>
+                            </div>
+                        </div>
+                        <div className="sub-searth">
+                            <TextField hiddenLabel variant="outlined" placeholder="검색어를 입력해주세요" />
+                            <IconButton type="button" aria-label="search" className="icon-search">
+                                <Search />
+                            </IconButton>
+                        </div>
+                    </div>
+                    <div className="sub-tab-content">
+                        <div className="radio-box">
+                            <FormControl>
+                                <RadioGroup
+                                    className="filter-radio"
+                                    row
+                                    aria-labelledby="radio-group"
+                                    name="group1"
+                                    value={radioGroup}
+                                    onChange={radioGroupChange}
+                                >
+                                    <FormControlLabel value="개인" control={<Radio />} label="개인" />
+                                    <FormControlLabel value="기업" control={<Radio />} label="기업" />
+                                </RadioGroup>
+                            </FormControl>
                         </div>
                         <div>
-                            <input type="text" />
+                            <FormControl>
+                                <RadioGroup
+                                    className="filter-radio"
+                                    row
+                                    aria-labelledby="radio-group"
+                                    name="group2"
+                                    value={radioGroup2}
+                                    onChange={radioGroupChange2}
+                                >
+                                    <FormControlLabel value="인기/추천" control={<Radio />} label="인기/추천" />
+                                    <FormControlLabel value="전체" control={<Radio />} label="전체" />
+                                    <FormControlLabel value="예적금" control={<Radio />} label="예적금" />
+                                    <FormControlLabel value="외환" control={<Radio />} label="외환" />
+                                    <FormControlLabel value="퇴직연금" control={<Radio />} label="퇴직연금" />
+                                    <FormControlLabel value="투자상품" control={<Radio />} label="투자상품" />
+                                    <FormControlLabel value="보험/방카" control={<Radio />} label="보험/방카" />
+                                    <FormControlLabel value="대출" control={<Radio />} label="대출" />
+                                    <FormControlLabel value="급여" control={<Radio />} label="급여" />
+                                    <FormControlLabel value="연금" control={<Radio />} label="연금" />
+                                    <FormControlLabel value="고자산" control={<Radio />} label="고자산" />
+                                </RadioGroup>
+                            </FormControl>
                         </div>
+                        <div>필터</div>
                     </div>
                 </div>
                 <div className="list-setting">
                     <div className="no-data">
-                        <strong>AI가 미리 생성해놓은 조건</strong>을 선택해서
-                        <br /> 타겟을 추출하세요.
+                        <div>
+                            <div className="no-data-img"></div>
+                            <strong>AI가 미리 생성해놓은 조건</strong>을 선택해서
+                            <br /> 타겟을 추출하세요.
+                        </div>
                     </div>
                 </div>
             </div>
